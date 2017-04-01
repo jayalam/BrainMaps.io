afa21cf8cc32368ef0e2ca20d205783ef0f4d884
Jorge Ayala <gorge.ayala@gmail.com>
2017-04-01 22:33:08 +0200

    Redux, ReactJS new approach from scratch

--- src/index.js
+++ src/index.js
@@ -2,12 +2,75 @@
  * Created by jorgeayala on 23/02/2017.
  */
 
+import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './app.css';
-import App from './app';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT' :
+            return state + 1;
+        case 'DECREMENT' :
+            return state - 1;
+        default :
+            return state;
+    }
+};
+
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+)
+
+
+const store = createStore(counter);
+
+const render = () => {
+    ReactDOM.render(
+        <Counter
+            value={ store.getState() }
+            onIncrement={ () =>
+                store.dispatch({
+                    type: 'INCREMENT'
+                })
+            }
+            onDecrement={ () =>
+                store.dispatch({
+                    type: 'DECREMENT'
+                })
+            }
+        />,
+        document.getElementById('root')
+    );
+};
+
+store.subscribe(render);
+render();
+
+const test = {
+    id: 0,
+    text: 'test!!',
+    completed: true
+};
+
+let toggle = (element) => {
+    // return Object.assign({}, element, {
+    //     completed : !element.completed
+    // });
+
+    return {
+        ...element,
+        completed: !element.completed
+    }
+};
+
+console.log(JSON.stringify(test, null, 4));
+
+console.log(JSON.stringify(toggle(test), null, 4));

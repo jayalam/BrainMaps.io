082c4e1338635651243334757247218a70a5b672
Jorge Ayala <gorge.ayala@gmail.com>
2017-04-02 04:36:45 +0200

    Get api call implemented. Search is not working, need to implement binding on the SearchBar. Check why search url is not working!

--- src/index.js
+++ src/index.js
@@ -5,13 +5,71 @@
 import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import request from 'request';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const counter = (state = 0, action) => {
+
+// TODO : check why the search is not working with limit and offset!!!!
+let url = 'http://lello.blopez.ec:9090/api/v1.0/competences/?top=true';
+// let url = 'http://lello.blopez.ec:9090/api/v1.0/competences/search/service';
+
+let listItems = {};
+
+const api_get = (url) => {
+
+    var options = {
+        url: url,
+        headers: {
+            'Accept-Language': 'en'
+        }
+    };
+
+    request.get(options, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            let items = JSON.parse(body).competences;
+
+            listItems = items.map((item) =>
+
+                <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                    <div className="d-flex w-100 justify-content-between">
+                        <h4 className="mb-1">{item.name.en}</h4>
+                    </div>
+                    <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+
+
+
+                    <p>Code : {item.code}</p>
+                </a>
+
+
+            );
+            console.log("loaded!");
+        }
+
+    });
+
+};
+
+
+let limit = 5;
+let offset = 0;
+api_get(url + '&limit=' + limit + '&offset=' + offset);
+offset += limit;
+
+
+const apiReducer = (state = [], action) => {
+
     switch (action.type) {
-        case 'INCREMENT' :
-            return state + 1;
-        case 'DECREMENT' :
-            return state - 1;
+        case 'API_GET' :
+            api_get(url + '&limit=' + limit + '&offset=' + offset);
+            offset += limit;
+            return [
+                ...state,
+                ...action.response
+            ];
+        case 'CLEAR':
+            offset = 0;
+            return [];
         default :
             return state;
     }
@@ -19,31 +77,64 @@ const counter = (state = 0, action) => {
 
 const Counter = ({
     value,
-    onIncrement,
-    onDecrement
+    getAPI,
+    clear
 }) => (
     <div>
-        <h1>{value}</h1>
-        <button onClick={onIncrement}>+</button>
-        <button onClick={onDecrement}>-</button>
+        <h1>BrainMaps.io</h1>
+        <p className="col-lg-10">
+            Welcome to BrainMaps.io the Search Engine for Compentencies!
+        </p>
+
+
+
+        <form onSubmit={e => {
+            e.preventDefault();
+        }}>
+
+            <div className="col-sm-10 form-horizontal">
+                {/* TODO : implement searchbar binding to API Call */}
+                <input id="searchBar" className="form-control" type="Text" placeholder="Search for..." />
+            </div>
+            <button className="btn btn-default" onClick={getAPI}>API GET</button>
+
+        </form>
+
+
+
+        <br/>
+
+
+
+        <div className="form-group col-lg-10">
+                <button className="btn btn-default" onClick={clear}>Clear All</button>
+        </div>
+
+        <br/>
+
+        <div className="list-group">{value}</div>
+
+
     </div>
-)
+);
 
 
-const store = createStore(counter);
+const store = createStore(apiReducer);
 
 const render = () => {
     ReactDOM.render(
         <Counter
             value={ store.getState() }
-            onIncrement={ () =>
+            getAPI={ () =>
                 store.dispatch({
-                    type: 'INCREMENT'
+                    type: 'API_GET',
+                    response: listItems
                 })
             }
-            onDecrement={ () =>
+            clear={ () =>
                 store.dispatch({
-                    type: 'DECREMENT'
+                    type: 'CLEAR',
+                    response: listItems
                 })
             }
         />,
@@ -53,24 +144,3 @@ const render = () => {
 
 store.subscribe(render);
 render();
-
-const test = {
-    id: 0,
-    text: 'test!!',
-    completed: true
-};
-
-let toggle = (element) => {
-    // return Object.assign({}, element, {
-    //     completed : !element.completed
-    // });
-
-    return {
-        ...element,
-        completed: !element.completed
-    }
-};
-
-console.log(JSON.stringify(test, null, 4));
-
-console.log(JSON.stringify(toggle(test), null, 4));
